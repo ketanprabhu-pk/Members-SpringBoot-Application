91be9b777591541dde4ae95b49b64f7455a2576d
ketanprabhu97 <ketanprabhu1297@gmail.com>
2021-12-10 15:29:07 +0530

    .

--- src/main/java/org/pk/members/controller/MemberController.java
+++ src/main/java/org/pk/members/controller/MemberController.java
@@ -2,11 +2,10 @@ package org.pk.members.controller;
 
 import org.pk.members.model.Member;
 import org.pk.members.service.MemberService;
+import org.pk.members.service.MembersException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +24,42 @@ public class MemberController {
     public Member getMemberById(@PathVariable String userid) {
         return memberService.getMemberById(userid);
     }
+
+    @RequestMapping(value = "/member", method = RequestMethod.POST, produces = {"application/json", "application/xml"})
+    public ResponseEntity<Object> addMember(@RequestBody Member member) throws MembersException {
+        if(memberService.addMember(member)){
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @RequestMapping(value = "/member/{userid}", method = RequestMethod.DELETE, produces = {"application/json", "application/xml"})
+    public ResponseEntity<Object> deleteMember(@PathVariable String userid) throws MembersException {
+        if(memberService.deleteMember(userid)){
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @RequestMapping(value = "/member/{userid}", method = RequestMethod.PUT, produces = {"application/json", "application/xml"})
+    public ResponseEntity<Object> updateMember(@PathVariable String userid, @RequestBody Member member) throws MembersException {
+        if(memberService.updateMember(userid, member)){
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @RequestMapping(value="/member/max-jobs", method = RequestMethod.GET, produces = {"application/json", "application/xml"})
+    public Member getMembersWithMaxJobs() {
+        return memberService.getMembersWithMaxJobs();
+    }
+
+    @RequestMapping(value="/member/remote", method = RequestMethod.GET, produces = {"application/json", "application/xml"})
+    public List<Member> getMembersWithRemote() {
+        return memberService.getMembersWithRemote();
+    }
+
 }
